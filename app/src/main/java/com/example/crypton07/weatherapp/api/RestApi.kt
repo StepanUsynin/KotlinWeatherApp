a66f648abec293685cc97c82499f194cfa7f0cf7
step <susynin@crypton.studio>
2018-09-27 14:54:59 +0700

    UI components added

--- app/src/main/java/com/example/crypton07/weatherapp/api/RestApi.kt
+++ app/src/main/java/com/example/crypton07/weatherapp/api/RestApi.kt
@@ -1,4 +1,17 @@
 package com.example.crypton07.weatherapp.api
 
-class RestApi {
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RestApi {    
+    private val baseUrl: String = "https://query.yahooapis.com/"    
+    fun create(): YahooWeatherApiService{
+        val retrofit = Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(baseUrl)
+                .build()
+        return retrofit.create(YahooWeatherApiService::class.java)
+    }
 }
--- app/src/main/java/com/example/crypton07/weatherapp/api/YahooWeatherApiService.kt
+++ app/src/main/java/com/example/crypton07/weatherapp/api/YahooWeatherApiService.kt
@@ -3,7 +3,7 @@ package com.example.crypton07.weatherapp.api
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface yahooWeatherApiService {
+interface YahooWeatherApiService {
     @GET("v1/public/yq")
     fun getData(@Query("q") query: String)
 }
