084e5cc808ce572bd0e70c932f38fe41a4bf46bb
Stepan Usynin <30403547+StepanUsynin@users.noreply.github.com>
2018-09-28 15:35:06 +0700

    Delete YahooWeatherApiService.kt

--- app/src/main/java/com/example/crypton07/weatherapp/api/YahooWeatherApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.crypton07.weatherapp.api
-
-import retrofit2.http.GET
-import retrofit2.http.Query
-
-interface YahooWeatherApiService {
-    @GET("v1/public/yq")
-    fun getData(@Query("q") query: String)
-}
